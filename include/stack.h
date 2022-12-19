3f9339c7b3a8665c72e687c92433287d0852ed23
Sokolov-Vladisl <elifantastika1.com>
2022-12-19 12:35:28 +0300

    for my friends

--- include/stack.h
+++ include/stack.h
@@ -1,3 +1,12 @@
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+#include<string>
+
+
+
+using namespace std;
+
 // объявление и реализация шаблонного стека
 // стек поддерживает операции: 
 // - вставка элемента, 
@@ -7,3 +16,79 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+
+template <typename T> class Tstack
+{
+private:
+
+	int top;
+	size_t memSize;
+	T* pMem;
+
+public:
+
+	Tstack(T* p, size_t in_size) : top(-1), memSize(in_size), pMem(p) {}
+
+	Tstack(size_t in_size) : top(-1), memSize(in_size)
+	{
+		pMem = new T[memSize];
+	}
+
+	Tstack(void) : top(-1), memSize(1)
+	{
+		pMem = new T[memSize];
+	}
+
+	/*	~Tstack()
+		{
+			//-|написать здесь что-то|--|написать здесь что-то|--|написать здесь что-то|--|написать здесь что-то|-
+		}
+	*/
+
+	~Tstack() = default;
+	//-|сделать свой деструктор|-
+
+	bool isEmpty() { return top == -1; }
+	bool isFull() { return top == memSize - 1; }
+
+	T Top() { return pMem[top]; }
+	size_t size() { return top + 1; }
+
+	void Push(T a)
+	{
+		if (isFull())
+		{
+			T* tmp = new T[memSize * 2];
+			for (size_t i = 0; i < memSize; i++)
+			{
+				tmp[i] = pMem[i];
+			}
+			delete[]pMem;
+			pMem = tmp;
+			memSize *= 2;
+			
+		}		
+			pMem[++top] = a;
+		
+	}
+
+	T Pop()
+	{
+		if (!isEmpty()) { return pMem[top--]; }
+
+	}
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,95 @@
-// реализация пользовательского приложения
+#include"arithmetic.h"
 
 int main()
 {
+
+	
+
+
+		string infix;
+
+		cout <<endl<< "Input :  ";
+		getline(cin, infix);
+		cout << endl << endl << "Your input without spaces :  ";
+		infix = without_spaces(infix);
+		
+
+//------------------------------------------------------------------------
+
+
+
+		
+
+//------------------------------------------------------------------------
+
+
+		cout << endl<<endl<<endl << "All operands in input";
+
+		cout << endl;
+		
+		
+		TPostfix<double> MainPart(infix);
+
+		
+
+
+
+		cout << endl << endl << endl;
+
+
+
+
+
+
+
+/*                 [ Test zone 1 ]             For Ivan, Grigory and Anton
+
+		Tstack<char> temp;
+
+		temp.Push('1');
+		temp.Push('2');
+		temp.Push('3');
+		temp.Push('4');
+		temp.Push('.');
+		temp.Push('5');
+		temp.Push('6');
+
+		Const tomp1(temp, temp, 1);
+		Const tomp2(temp, temp, 1);
+		Const tomp3(temp, temp, -1);
+		Const tomp4(temp, temp, 1);
+		Const tomp5(temp, temp, -1);
+
+		LEXEM mas[5];
+
+		mas[0] = tomp1;
+		mas[1] = tomp2;
+		mas[2] = tomp3;
+		mas[3] = tomp4;
+		mas[4] = tomp5;
+
+		for (int i = 0; i < 5; i++)
+		{
+			cout << mas[i].show()<<endl;
+		}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return 0;
 }
