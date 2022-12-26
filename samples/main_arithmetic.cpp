9cb2d644f7e01732d273ff65ff8b6cab3fda2b10
NizorS <95180832+NizorS@users.noreply.github.com>
2022-12-26 17:45:24 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,56 +1,33 @@
-//Объявление начальных библиотек
+#include <iomanip>
 #include "arithmetic.h"
 
+using std::cout;
+using std::endl;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Реализация пользовательского приложения
-//Правила ввода арифмитического выражения
 int main()
 {
-	cout << "Rule for entering your arithmetic expression: " << endl;
-	cout << "|Possible operations  +, -, *, / . You can use parentheses for simplicity. When entering the entered numbers used points instead of a comma(0.0, 1.0, 4.2)|" << endl;
-	cout << "|Please , enter your arithmetic expression (no spaces). Use latin to enter your users|" << endl;
-	cout << "|Possible operations  +, -, *, / . You can use parentheses for simplicity. When entering the entered numbers used points instead of a comma|" << endl;
+	cout << "The program supports:" << endl;
+	cout << "- Unary ('-') and binary ('+', '-', '*', '/') operations" << endl;
+	cout << "- Brackets" << endl;
+	cout << "- Integer and real constants" << endl;
+	cout << "- Decimal separator only as a point: '.'" << endl;
+	cout << "- Exponential form of any signed ('-' or '+') or unsigned numbers" << endl;
+	cout << "- Variable names of any length using only lowercase Latin characters, excluding 'e'" << endl;
+	cout << "- Prohibition of spaces" << endl;
 
-	string infix;
-	TPostfix expression;
-
-
-	//Ввод арифмитического выражанеия
-	while (1)
+	string source;
+	cout << endl << "Infix form: ";
+	std::getline(std::cin, source);  // space-controlled input
+	try
 	{
-		for (size_t i = 0; i < 116; i++)
-			cout << "_";
-		cout << endl;
-		cout << "|Please , enter your arithmetic expression (no spaces)|: ";
-		cin >> infix;
-
-		try
-		{
-      expression.toPostfix();
-			cout << "Postfix form: " << expression.getPostfix() << endl;
-			expression.setTPostfix(infix);
-			cout << "Infix form: " << expression.getInfix() << endl;
-			
-			expression.toCalculate();
-			cout << setprecision(10) << "Output result: " << expression.getResult() << endl;
-		}
-		catch (const char* msg)
-		{
-			expression.getError();
-			cout << msg << endl;
-		}
+		TArithmetic expression(source);
+		cout << endl << "Postfix form: " << expression.GetPostfix() << endl;
+		cout << endl << "Expression value: " << expression.Calculate() << endl;
 	}
+	catch (const string message)
+	{
+		cout << endl << message << endl;
+	}
+
+	return 0;
 }
