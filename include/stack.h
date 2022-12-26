6b6234bd70837648521b085157dfe8644be19161
NizorS <95180832+NizorS@users.noreply.github.com>
2022-12-26 17:15:45 +0300

    fix

--- include/stack.h
+++ include/stack.h
@@ -1,9 +1,95 @@
-// объявление и реализация шаблонного стека
-// стек поддерживает операции: 
-// - вставка элемента, 
-// - извлечение элемента, 
-// - просмотр верхнего элемента (без удаления)
-// - проверка на пустоту, 
-// - получение количества элементов в стеке
-// - очистка стека
-// при вставке в полный стек должна перевыделяться память
+//РћР±СЉСЏРІР»РµРЅРёРµ РЅР°С‡Р°Р»СЊРЅС‹С… Р±РёР±Р»РёРѕС‚РµРє
+#pragma once
+
+
+
+
+
+//РќР°С‡Р°Р»Рѕ СЂР°Р±РѕС‚С‹ РєР»Р°СЃСЃР°
+template<class T>
+class Stack
+{
+private:
+	int index = -1;
+	size_t dataSize;
+	T* data;
+public:
+	Stack()
+	{
+		dataSize = 5;
+		data = new T[dataSize];
+	}
+
+	Stack(const Stack& temp)
+	{
+		this->index = temp.index;
+		this->dataSize = temp.dataSize;
+		this->data = new T[this->dataSize];
+
+		for (int i = 0; i < this->dataSize; i++)
+			this->data[i] = temp.data[i];
+	}
+
+	~Stack()
+	{
+		delete[] data;
+	}
+
+	bool isEmpty() 
+	{
+		return index == -1;
+	}
+
+	void push(T temp) 
+	{
+		if ((index + 1) == dataSize)
+		{
+			size_t tempSize = this->dataSize;
+			T* temp = new T[tempSize];
+			for (size_t i = 0; i < tempSize; i++)
+				temp[i] = this->data[i];
+
+			delete[] this->data;
+
+			dataSize *= 2;
+			this->data = new T[dataSize];
+			for (int i = 0; i < tempSize; i++)
+				data[i] = temp[i];
+
+			delete[] temp;
+		}
+
+		data[++index] = temp;
+	}
+
+
+
+	//РџСЂРѕСЃРјРѕС‚СЂ РІРµСЂС…РЅРµРіРѕ СЌР»РµРјРµРЅС‚Р°
+	T pop() 
+	{
+		if (!isEmpty())
+			return data[index--];
+		throw std::out_of_range("Stack is empty");
+	}
+
+	T top() 
+	{
+		if (!isEmpty())
+			return data[index];
+		throw std::out_of_range("Stack is empty");
+	}
+
+
+
+	int size() 
+	{
+		return index + 1;
+	}
+
+	//РћС‡РёСЃС‚РєР° СЃС‚РµРєР°
+	void clear() 
+	{
+		index = -1;
+	}
+
+};
