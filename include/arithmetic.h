c906430d81911f55b18557bdd1646f0f7af348a9
NizorS <95180832+NizorS@users.noreply.github.com>
2022-12-26 18:14:57 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,135 +1,85 @@
-#include "arithmetic.h"
-#include <gtest.h>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <locale>
+#include <map>
+#include <ostream>
 
-TEST(TPostfix, can_create_tpostfix_without_infix)
+class Lex
 {
-    ASSERT_NO_THROW(TPostfix test);
-}
+protected:
+	std::string name;
+	std::string lexem;
+	double double_spot;
+	int pr;
 
-TEST(TPostfix, can_create_tpostfix_with_infix)
+public:
+	std::string LexType();//vozvrashaet tip lexem
+	std::string GetLex();//vozvrashaet lexem
+	virtual void SetPrior(char op) = 0;
+	int GetPrior();
+	double value();
+};
+
+class operations :public Lex
 {
-    ASSERT_NO_THROW(TPostfix test("5+5"));
-}
+protected:
+	void SetPrior(char op) override;//prioretet operaciy + - prior 1; * / prior =2 ;~ prior 3
+public:
+	operations(char op);
+};
 
-TEST(TPostfix, can_set_other_infix)
+class operands : public Lex
 {
-    TPostfix test("5+5");
-    ASSERT_NO_THROW(test.setTPostfix("5"));
-}
-
-TEST(TPostfix, can_convert_to_lexem_array)
+protected:
+	double NumConv(std::string strlex);
+public:
+	void SetPrior(char op);
+};
+class Num : public operands
 {
-    TPostfix test("56.2+576.E89");
-    string array[3] = {"56.2", "+", "576.E89"};
-    for (size_t i = 0; i < 3; i++)
-        EXPECT_EQ(test.getLexem(i), array[i]);
-}
-
-TEST(TPostfix, throw_if_opening_bracket_in_wrong_position)
+public:
+	Num(std::string lex);
+	Num(double value);
+	Num();
+};
+class Var : public operands
 {
-    ASSERT_ANY_THROW(TPostfix test("9("));
-}
+public:
+	Var(std::string lex);
+};
 
-TEST(TPostfix, throw_if_closing_bracket_in_wrong_position)
+class Arithmetic
 {
-    ASSERT_ANY_THROW(TPostfix test("-)*9"));
-}
+private:
+	std::string infix;
+	int size;//videlyaemaya pamyat'
+	int postfix_size;//kol-vo elem v postfix'e
+	int lex_size = -1;//kol-vo elem v lexem 
+	Lex** lexem = nullptr;
+	Lex** postfix = nullptr;
 
-TEST(TPostfix, throw_if_operation_in_wrong_position)
-{
-    ASSERT_ANY_THROW(TPostfix test("++9"));
-}
+	bool IsOperation(char v);
+	bool CheckOp();
+	bool IsOperand(const char& lexem);
+	bool IsDigit(const char& ch);
+	bool IsNumber(std::string num);
+	bool IsVar(const char& ch);
+	bool IsVariable(std::string num);
+	void CheckBrackets();
+	bool IsBrackets(const char& ch);
+	//void CorrectOrder();
 
-TEST(TPostfix, throw_if_minus_in_wrong_position)
-{
-    ASSERT_ANY_THROW(TPostfix test("(9)-"));
-}
+	void resize();
 
-TEST(TPostfix, throw_if_name_variables_incorrect)
-{
-    ASSERT_ANY_THROW(TPostfix test("T"));
-    ASSERT_ANY_THROW(TPostfix test("aa"));
-}
+	void IncorrectSymbols();
+	void VarValue();
 
-TEST(TPostfix, throw_if_exponential_notation_is_wrong)
-{
-    TPostfix test;
-    ASSERT_ANY_THROW(test.setTPostfix("9E+6"));
-}
+	void Postfix();
+	void Parser();
 
-TEST(TPostfix, throw_if_number_notation_is_wrong)
-{
-    TPostfix test;
-    ASSERT_ANY_THROW(test.setTPostfix(".6"));
-    ASSERT_ANY_THROW(test.setTPostfix("6."));
-}
-
-TEST(TPostfix, throw_if_bracket_number_isnt_equal)
-{
-    TPostfix test;
-    ASSERT_ANY_THROW(test.setTPostfix("((9+6)"));
-}
-
-TEST(TPostfix, correct_operation_priority)
-{
-    TPostfix test;
-    EXPECT_EQ(test.operation_priority("+"), 1);
-    EXPECT_EQ(test.operation_priority("-"), 1);
-    EXPECT_EQ(test.operation_priority("*"), 2);
-    EXPECT_EQ(test.operation_priority("/"), 2);
-    EXPECT_EQ(test.operation_priority("~"), 3);
-    EXPECT_EQ(test.operation_priority("("), 0);
-}
-
-TEST(TPostfix, correct_converting_string_to_number)
-{
-    TPostfix test;
-    EXPECT_EQ(test.toNumber("96.678"), 96.678);
-    EXPECT_EQ(test.toNumber("-96"), -96);
-    double temp;
-    temp = floor(test.toNumber("-96.45") * pow(10.0, 16)) / pow(10.0, 16);
-    EXPECT_EQ(temp, -96.45);
-    temp = floor(test.toNumber("-9.456E2") * pow(10.0, 16)) / pow(10.0, 16);
-    EXPECT_EQ(temp, -945.6);
-}
-
-TEST(TPostfix, correct_converting_to_postfix_form)
-{
-    string temp = "6989+";
-    TPostfix test("69+89");
-    test.toPostfix();
-    EXPECT_EQ(test.getPostfix(), temp);
-
-    temp = "896978+8989~*-*9+";
-    test.setTPostfix("89*(69+78-89*-89)+9");
-    test.toPostfix();
-    EXPECT_EQ(test.getPostfix(), temp);
-}
-
-TEST(TPostfix, correct_calculating)
-{
-    double temp = 69.0 + 89.0;
-    TPostfix test("69+89");
-    test.toPostfix();
-    test.toCalculate();
-    EXPECT_EQ(test.getResult(), temp);
-
-    temp = 89 * (69 + 78 - 89 * (-89)) + 9;
-    test.setTPostfix("89*(69+78-89*-89)+9");
-    test.toPostfix();
-    test.toCalculate();
-    EXPECT_EQ(test.getResult(), temp);
-
-    temp = 5 - (-(-(-(4))));
-    test.setTPostfix("5----4");
-    test.toPostfix();
-    test.toCalculate();
-    EXPECT_EQ(test.getResult(), temp);
-}
-
-TEST(TPostfix, throw_if_division_by_zero)
-{
-    TPostfix test("8/0");
-    test.toPostfix();
-    EXPECT_ANY_THROW(test.toCalculate(););
-}
+public:
+	Arithmetic(std::string arithmetic);
+	double Calculate();
+	~Arithmetic();
+};
