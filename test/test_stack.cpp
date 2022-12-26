5c3fef7ccd61c9354b8fb68ff2a56ba409c9e9be
NizorS <95180832+NizorS@users.noreply.github.com>
2022-12-26 17:34:48 +0300

    fix

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -1,19 +1,11 @@
-//Начальные библиотеки
 #include "stack.h"
 #include <gtest.h>
 
-
-
-
-
-
-
-
-// Тесты для стека
 TEST(Stack, can_create_stack)
 {
     ASSERT_NO_THROW(Stack<int> stack);
 }
+
 TEST(Stack, can_create_copied_stack)
 {
     Stack<int> stack1;
@@ -24,6 +16,7 @@ TEST(Stack, can_create_copied_stack)
     for (int i = 0; i < stack2.size(); i++)
         EXPECT_EQ(stack1.pop(), stack2.pop());
 }
+
 TEST(Stack, copied_stack_is_equal_to_source_one)
 {
     Stack<int> stack1;
@@ -33,6 +26,7 @@ TEST(Stack, copied_stack_is_equal_to_source_one)
     for (int i = 0; i < stack2.size(); i++)
         EXPECT_EQ(stack1.pop(), stack2.pop());
 }
+
 TEST(Stack, copied_stack_has_its_own_memory)
 {
     Stack<int> stack1;
@@ -42,23 +36,27 @@ TEST(Stack, copied_stack_has_its_own_memory)
     for (int i = 0; i < stack1.size(); i++)
         EXPECT_NE(stack1.pop(), stack2.pop());
 }
+
 TEST(Stack, can_get_stack_element_number)
 {
     Stack<int> stack;
     ASSERT_NO_THROW(stack.size());
     EXPECT_EQ(0, stack.size());
 }
+
 TEST(Stack, true_when_stack_is_empty)
 {
     Stack<int> stack;
     EXPECT_EQ(true, stack.isEmpty());
 }
+
 TEST(Stack, false_when_stack_isnt_empty)
 {
     Stack<int> stack;
     stack.push(1);
     EXPECT_EQ(false, stack.isEmpty());
 }
+
 TEST(Stack, can_set_and_get_element)
 {
     Stack<int> stack;
@@ -67,17 +65,20 @@ TEST(Stack, can_set_and_get_element)
     ASSERT_NO_THROW(stack.pop());
     EXPECT_EQ(4, stack.pop());
 }
+
 TEST(Stack, throws_when_get_element_in_empty_stack)
 {
     Stack<int> stack;
     ASSERT_ANY_THROW(stack.pop());
 }
+
 TEST(Stack, other_index_after_push)
 {
     Stack<int> stack;
     stack.push(1);
     EXPECT_EQ(0, stack.size() - 1);
 }
+
 TEST(Stack, more_memory_after_push)
 {
     Stack<int> stack;
@@ -85,12 +86,14 @@ TEST(Stack, more_memory_after_push)
         stack.push(i);
     ASSERT_NO_THROW(stack.push(1));
 }
+
 TEST(Stack, get_top_element)
 {
     Stack<int> stack;
     stack.push(1);
     EXPECT_EQ(1, stack.top());
 }
+
 TEST(Stack, method_top_dont_change_stack_index)
 {
     Stack<int> stack;
@@ -98,11 +101,13 @@ TEST(Stack, method_top_dont_change_stack_index)
     stack.top();
     EXPECT_EQ(0, stack.size() - 1);
 }
+
 TEST(Stack, throws_when_get_top_element_in_empty_stack)
 {
     Stack<int> stack;
     ASSERT_ANY_THROW(stack.top());
 }
+
 TEST(Stack, can_clean_stack)
 {
     Stack<int> stack;
