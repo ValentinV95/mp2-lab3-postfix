1cd228ac792642536c7d4cdf803ce824d4cfc37c
KDerevyankin <95866902+KDerevyankin@users.noreply.github.com>
2022-12-26 21:50:20 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,39 @@
-// объявление функций и классов для вычисления арифметических выражений
+// РѕР±СЉСЏРІР»РµРЅРёРµ С„СѓРЅРєС†РёР№ Рё РєР»Р°СЃСЃРѕРІ РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРёС… РІС‹СЂР°Р¶РµРЅРёР№
+
+#ifndef _ARIFMETIC_H_
+#define _ARIFMETIC_H_
+
+#include <iostream>
+#include <stdexcept>
+#include "string"
+#include "stack.h"
+#include "vector"
+#include "map"
+
+class Postfix 
+{
+protected:
+	int isOperator(char);
+	void checkBrackets(const std::string&)const;
+	int checkingLine(const std::string&)const;
+public:
+	std::string ToPostfix(const std::string&)const;
+	double calculate(const std::string&, std::map<char, double>&);
+};
+
+class myExcp : public std::exception
+{
+private:
+	std::string error;
+public:
+	myExcp(std::string s)
+	{ 
+		error = s; 
+	}
+	const char* what() const noexcept
+	{
+		return error.c_str();
+	}
+};
+
+#endif
