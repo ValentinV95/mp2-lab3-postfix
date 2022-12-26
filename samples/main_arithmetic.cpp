3cc2ac17e70c02abe9f73dd4856a8628eb947e62
NizorS <95180832+NizorS@users.noreply.github.com>
2022-12-26 18:06:39 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,23 +1,12 @@
-// реализация пользовательского приложения
-#include<Arithmetic.h>
-#include<string>
-#include<iostream>
-int main()//Пользовательское приложение
-{
-	std::string str;
-	std::cout << "Input Infix form"<<'\n';
-	std::cin >> str;
-	std::cout <<"This is your infix form" <<'\n'<< str;
-	try
-	{
-		Arithmetic ar(str);
-		double res = ar.Calculate();
-		std::cout <<"\n\n"<<"This is your answer"<<'\n' << res;
-	}
-	catch (const std::string& error)
-	{
-		std::cout << error;
-	}
+#include <iostream>
+#include "arithmetic.h"
 
-
+int main()
+{	
+	string infix;
+	cout << "supported operations: +, -, *, /, (, )" << endl;
+	cout << "Supports the introduction of variables and double" << endl;
+	cout << "Enter infix form" << endl;
+	cin >> infix;
+	TPostfix f(infix);
 }
