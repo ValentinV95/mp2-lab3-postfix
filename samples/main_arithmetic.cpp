bf664fca80b24ecc430808dba3a7dbd58183a48d
NizorS <95180832+NizorS@users.noreply.github.com>
2022-12-26 18:43:24 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,21 +1,30 @@
+//Объявление билиотек
 #include<Arithmetic.h>
 #include<string>
 #include<iostream>
+#include <math.h>
+#include <locale>
+#include <map>
+#include <ostream>
+
+
+
+//Меню пользователья
 int main()//Пользовательское приложение
 {
 	std::string str;
-	std::cout << "Input Infix form"<<'\n';
+	std::cout << "Send Infix form"<<'\n';
 	std::cin >> str;
-	std::cout <<"This is your infix form" <<'\n'<< str;
+	std::cout <<"Infix form" <<'\n'<< str;
 	try
 	{
 		Arithmetic ar(str);
 		double res = ar.Calculate();
-		std::cout <<"\n\n"<<"This is your answer"<<'\n' << res;
+		std::cout <<"\n\n"<<"Result:"<<'\n' << res;
 	}
 	catch (const std::string& error)
 	{
-		std::cout << error;
+		std::cout << Wrong record of an arithmetic expression or something else;
 	}
 
 
