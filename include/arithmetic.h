1da26c643a195127bfd2ff7cd8d6877556cd46bb
NizorS <95180832+NizorS@users.noreply.github.com>
2022-12-26 17:46:25 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,69 +1,83 @@
-// Объявление функций и классов для вычисления арифметических выражений
-
-
-//Объявление начальных библиотек
-
-
-using namespace std;
 #pragma once
+
 #include <iostream>
-#include <iomanip>
-#include "stack.h"
+#include <map>
+#include <cmath>
 #include <string>
+#include "stack.h"
 
+using std::string;
 
-
-
-//Начало работы класса
-
-class TPostfix
+class TArithmetic
 {
-// Используваемые   класса для работы программы (Закрытые)
 private:
-	size_t size;
-	string infix, postfix;
-	string* data;
-	Stack<string> operations;
-	Stack<double> numbers;
-	double result;
-	int errornumber;
-	string errorstring;
+	class Lexem
+	{
+	protected:
+		string label;
+		string lex;
+		double value;
 
+	public:
+		const string Get() const;
+		const double Value() const;
+		const string what() const;
+		virtual void SetValue(const string& val) = 0;
+		virtual ~Lexem();
+	};
 
+	class Operation : public Lexem
+	{
+	private:
+		void SetValue(const string& val) override;
 
-// Используваемые  класса для работы программы (Открытые)
+	public:
+		Operation(const char& data);
+	};
+
+	class Operand : public Lexem
+	{
+	protected:
+		double toNumeric(const string& strlex);
+	};
+
+	class Constant : public Operand
+	{
+	private:
+		void SetValue(const string& val) override;
+
+	public:
+		Constant(const string& data);
+	};
+
+	class Variable : public Operand
+	{
+	public:
+		Variable(const string& data);
+		void SetValue(const string& val) override;
+	};
+
+	string infix;
+	Lexem** lexems;
+	Lexem** postfix;
+
+	bool find_operation(const char& tmplex);
+	bool isDigit(const char& tmplex);
+	bool isAlpha(const char& tmplex);
+
+	const string find_error();
+	void parser();
+	void toPostfix();
+	bool check_input(const string& inp);
+
+	int lexems_count = 0;
+	int operations_count = 0;
+	int postfix_count = 0;
 
 public:
-
-	TPostfix();
-
-	TPostfix(string _infix); 
-
-	~TPostfix(); 
-	void setTPostfix(string _infix); 
-
-	void toLexem();
-
-	void stringCheck(string it); 
-
-	void toVariable(); 
-
-	size_t operation_priority(string operation); 
-
-	void toPostfix(); 
-
-	double toNumber(string number); 
-
-	void toCalculate(); 
-
-	string getPostfix(); 
-
-	double getResult(); 
-
-	string getInfix(); /
-
-	string getLexem(int n); 
-
-	void getError(); 
+	TArithmetic(const string& inf);
+	const string GetPostfix() const;
+	const double Calculate();
+	~TArithmetic();
 
 };
